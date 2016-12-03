cb6e397789fe3a7a2488218d89779e7ae0098818
Johan Lipecki <lipecki@kth.se>
2016-12-03 16:55:57 +0100

    controller

--- Lab1/src/view/MovieController.java
+++ Lab1/src/view/MovieController.java
@@ -3,28 +3,53 @@
  */
 package src.view;
 
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.KeyStroke;
+
 
 /**
  * @author jlipecki
  *
  */
-public class MovieController implements ActionListener, Runnable {
+public class MovieController {
+	public class CloseWindow implements ActionListener{
 
-	/* (non-Javadoc)
-	 * @see java.lang.Runnable#run()
-	 */
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-
+		public void actionPerformed(ActionEvent e) {
+		    final Action exit = new AbstractAction("exit") {
+	
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	            	JFrame f = new JFrame();
+					if(e.getSource() instanceof JFrame) f = (JFrame) e.getSource();
+	            	
+	                f.dispatchEvent(new WindowEvent(
+	                    f, WindowEvent.WINDOW_CLOSING));
+	            }
+	        };
+		}
 	}
+	
+	public class addMovie implements ActionListener{
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
 		
 	}
-
 }
